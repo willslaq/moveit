52d50d19977003fade38fb0060a0dafdba99b0dd
willslaq <willslaqtomaz@gmail.com>
2021-02-25 07:10:55 -0300

    Concluido ciclo de vida do contador e gerando novos desafios dinamicamente, ainda não está implementado avanço de level e não é possível concluir o exercício, apenas falhar :D #neverstoplearning

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,30 @@
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar'
 import { Profile } from '../components/Profile';
 
+import Head from 'next/head';
+
 import styles from '../styles/pages/Home.module.css';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | MoveIt</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
+          <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
